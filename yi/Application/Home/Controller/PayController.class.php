05f19d8d9d1ff20ae0803e394516343e3bc4e16e
muzhitao <695753037@qq.com>
2017-03-09 16:47:33 +0800

    Alipay

--- yi/Application/Home/Controller/PayController.class.php
+++ yi/Application/Home/Controller/PayController.class.php
@@ -10,5 +10,24 @@ namespace Home\Controller;
 use Think\Controller;
 
 class PayController extends BaseController {
+	
+	public function _initialize() {
+		vendor('Alipay.Corefunction');
+        vendor('Alipay.Md5function');
+        vendor('Alipay.Notify');
+        vendor('Alipay.Submit');    
+	}
+	
+	public function doalipay() {
+		
+	}
+	
+	public function notifyurl() {
+		
+	}
+	
+	public function returnurl() {
+		
+	}
 
 }
